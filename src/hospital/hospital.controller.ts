1084aca52b559317f9426ca9652b4950ae394434
bupadhyay <upadhyaybs@live.com>
2022-10-14 14:10:00 -0500

    Removed lock files

--- src/hospital/hospital.controller.ts
+++ src/hospital/hospital.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { HospitalService } from './hospital.service';
 import { CreateHospitalDto } from './dto/create-hospital.dto';
 import { UpdateHospitalDto } from './dto/update-hospital.dto';
