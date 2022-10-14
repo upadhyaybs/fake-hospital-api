a259c642abd42924235408364086405345fe3bdc
bupadhyay <upadhyaybs@live.com>
2022-10-14 14:20:17 -0500

    Restructured packages

--- src/hospital/controller/hospital.controller.spec.ts
+++ src/hospital/controller/hospital.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HospitalController } from './hospital.controller';
-import { HospitalService } from './hospital.service';
+import { HospitalService } from '../hospital.service';
 
 describe('HospitalController', () => {
   let controller: HospitalController;
