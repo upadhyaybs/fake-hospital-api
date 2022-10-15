cf35f20da557d3d3f3a0b6761233f7389fc3a653
bupadhyay <upadhyaybs@live.com>
2022-10-15 12:48:35 -0500

    Test Build Trigger

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Hospital!';
+    return 'Hola ! Welcome to Hospital API!';
   }
 }
